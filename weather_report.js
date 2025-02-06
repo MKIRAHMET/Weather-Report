a406d9f0857e5fe7f8485d341047282ebfc2acd3
Muhammed <muhammed@192.168.2.16>
2025-02-06 23:09:48 +0200

    v1

---
+++ weather_report.js
@@ -0,0 +1,19 @@
+function showweatherDetails(event) {
+    event.preventDefault();
+
+
+const city = document.getElementById('city').value;
+const apiKey = 'cd4951d1205c152ffb10948ef6eeef8c'
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `
+            <h2>Weather in ${data.name}</h2>
+            <p>Temperature: ${data.main.temp}°C</p>
+            <p>Description: ${data.weather[0].description}</p>`;
+    });
+}
+document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
